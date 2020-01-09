3374a5dab0e6f9536c4a9762139ba6224ad4c4fa
715484127 <715484127@qq.com>
2020-01-09 21:03:28 +0800

    单例demo

---
+++ src/main/java/com/javacore/demo/passvalue/PassValueDemo.java
@@ -0,0 +1,36 @@
+package com.javacore.demo.passvalue;
+
+import java.util.Arrays;
+
+/**
+ * Java方法的参数传递
+ * @author wangyuepeng
+ */
+public class PassValueDemo {
+    public static void main(String[] args) {
+        int i = 1;
+        String str = "hello";
+        Integer num = 200;
+        int[] arr = {1,2,3,4,5};
+        MyData my = new MyData();
+
+        change(i,str,num,arr,my);
+
+        System.out.println("i = " + i);
+        System.out.println("str = " + str);
+        System.out.println("num = " + num);
+        System.out.println("arr = " + Arrays.toString(arr));
+        System.out.println("my.a = " + my.a);
+    }
+    public static void change(int j, String s, Integer n, int[] a,MyData m){
+        j += 1;
+        s += "world";
+        n += 1;
+        a[0] += 1;
+        m.a += 1;
+    }
+}
+
+class MyData{
+    int a = 10;
+}
